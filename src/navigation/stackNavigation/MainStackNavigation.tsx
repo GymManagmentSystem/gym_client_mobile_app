55993b6f1993fdb3bf5c34b927de95fa36d4d4ce
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-09 23:33:23 +0530

    moving navigation to src

--- src/navigation/stackNavigation/MainStackNavigation.tsx
+++ src/navigation/stackNavigation/MainStackNavigation.tsx
@@ -1,5 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from '../../src/screens/LoginScreen';
+import LoginScreen from '../../screens/LoginScreen';
+
 
 export type MainStackNavigationList = {
   LoginScreen: undefined;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { View } from 'react-native'
 import ThemeText from '../components/ThemeText'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { useNavigation } from '@react-navigation/native'
+import { MainStackNavigationList } from '../navigation/stackNavigation/MainStackNavigation'
+
+
+type LoginScreenNavigationProp=NativeStackNavigationProp<MainStackNavigationList,"LoginScreen">
 
 const LoginScreen = () => {
+
+  const navigation=useNavigation<LoginScreenNavigationProp>()
+
   return (
     <View>
         <ThemeText fontSize='large'>Hello world</ThemeText>
