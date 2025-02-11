ad0cd889736e2bb045fbba5686e075a6bbbcc0d9
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-11 16:06:48 +0530

    adding Use Login Custom Hook

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from './src/context/ThemeContext';
 import MainStackNavigation from './src/navigation/stackNavigation/MainStackNavigation';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+const client = new QueryClient();
 
 const App = () => {
   return (
     <ThemeProvider>
-      <NavigationContainer>
-        <MainStackNavigation />
-      </NavigationContainer>
+      <QueryClientProvider client={client}>
+        <NavigationContainer>
+          <MainStackNavigation />
+        </NavigationContainer>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
