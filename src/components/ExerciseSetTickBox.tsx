541d0afe74001305653009cf1506a1134a1bac08
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-16 19:13:58 +0530

    adding Dynamic Disable Exercise Tracking Card

--- src/components/ExerciseSetTickBox.tsx
+++ src/components/ExerciseSetTickBox.tsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import { Image, StyleSheet, TouchableOpacity } from 'react-native'
-import { getHeightPercentage, getWidthPercentage } from '../utility/Dimensions'
-import { useTheme } from '../context/ThemeContext'
+import React, {useState} from 'react';
+import {Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {getHeightPercentage, getWidthPercentage} from '../utility/Dimensions';
+import {useTheme} from '../context/ThemeContext';
 
-
-const ExerciseSetTickBox = () => {
-
-    const theme=useTheme()
-
-
-  return (
-    <TouchableOpacity style={[style.tickBoxContainer,{backgroundColor:theme.colors.background.other}]} disabled={true}>
-        <Image source={require('../../assets/icons/tickIcon.png')} style={style.image}/>
-    </TouchableOpacity>
-  )
+interface ExerciseSetTickBoxProps {
+  isDisable: boolean;
 }
 
-export default ExerciseSetTickBox
+const ExerciseSetTickBox = ({isDisable}: ExerciseSetTickBoxProps) => {
+  const theme = useTheme();
+  const [showImage, setShowImage] = useState<boolean>(false);
+  const backgroundColor = isDisable
+    ? theme.colors.background.quaternary.primary
+    : theme.colors.background.other;
 
-const style=StyleSheet.create({
-    tickBoxContainer:{
-        width:getWidthPercentage(30),
-        height:getHeightPercentage(30),
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        borderRadius:5
-    },
-    image:{
-        width:"100%",
-        height:"100%"
-    }
-})
+  return (
+    <TouchableOpacity
+      style={[style.tickBoxContainer, {backgroundColor}]}
+      disabled={isDisable}
+      onPress={() => setShowImage(true)}>
+      {showImage && (
+        <Image
+          source={require('../../assets/icons/tickIcon.png')}
+          style={style.image}
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default ExerciseSetTickBox;
+
+const style = StyleSheet.create({
+  tickBoxContainer: {
+    width: getWidthPercentage(30),
+    height: getHeightPercentage(30),
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
