1f9908474f7f895fb817a275cfcea1c6792ba5e3
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-20 18:08:35 +0530

    adding Progress Circle

---
+++ src/components/ProgressCircle.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Alert, View } from 'react-native'
+import * as Progress from 'react-native-progress';
+
+const ProgressCircle = () => {
+  return (
+    <View>
+      <Progress.Circle size={120}  progress={0.5} color='#F1B900' unfilledColor='#8F918F'  thickness={20}/>
+    </View>
+  )
+}
+
+export default ProgressCircle
