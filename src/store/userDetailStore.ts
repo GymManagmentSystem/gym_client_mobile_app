341a42b8485ed3126fca591df009e8ff7be69b60
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-22 14:38:11 +0530

    adding User Last Login State

--- src/store/userDetailStore.ts
+++ src/store/userDetailStore.ts
@@ -3,15 +3,19 @@ import {create} from 'zustand'
 interface UserDataStore{
     loggedUserName:string,
     loggedMmeberId:number,
+    isFirstUserLogin:boolean,
     setUserName:(name:string)=>void
     setLoggedMemberId:(id:number)=>void
+    setFirstUserLogin:(status:boolean)=>void
 }
 
 const useUserDataStore=create<UserDataStore>(set=>({
     loggedUserName:"",
     loggedMmeberId:0,
+    isFirstUserLogin:false,
     setUserName:(userName:string)=>set(()=>({loggedUserName:userName})),
-    setLoggedMemberId:(userId:number)=>set(()=>({loggedMmeberId:userId}))
+    setLoggedMemberId:(userId:number)=>set(()=>({loggedMmeberId:userId})),
+    setFirstUserLogin:(status:boolean)=>set(()=>({isFirstUserLogin:status}))
 }))
 
 export default useUserDataStore;
