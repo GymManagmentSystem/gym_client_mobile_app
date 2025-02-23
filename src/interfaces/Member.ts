76b3e950314d241a3c4875b0dc91e52bdcbbf683
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2025-02-23 22:18:36 +0530

    getting Member Details By Id

---
+++ src/interfaces/Member.ts
@@ -0,0 +1,9 @@
+export interface Member{
+    firstName:string,
+    lastName:string,
+    age:number,
+    contactNumber:string,
+    email:string,
+    dateRegistered:string,
+    profileImageUrl:string
+}
